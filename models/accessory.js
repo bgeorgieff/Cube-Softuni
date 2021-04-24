5163997c4343e94f5ded228aba3a4e0bc748c2de
Blagovest Georgiev <b.g.georgiev87@gmail.com>
2021-04-24 19:51:02 +0300

    feat: add and manage accessories page

--- models/accessory.js
+++ models/accessory.js
@@ -20,4 +20,8 @@ const AccessorySchema = new mongoose.Schema({
     }]
 })
 
-model.exports = mongoose.model('Accessory', AccessorySchema)
+// AccessorySchema.path('imageUrl').validate(function (url) {
+//     return url.startsWith('http://') || url.startsWith('https"//')
+// }, 'Image URL is not valid')
+
+module.exports = mongoose.model('Accessory', AccessorySchema)
